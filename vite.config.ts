8d5f2caa6dcc0c5f75d17a27246d6e446c894e95
notnoobmaster <42277794+notnoobmaster@users.noreply.github.com>
2024-02-18 12:12:35 +0200

    Publish at /fsm-game

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "fsm-game",
+  base: "/fsm-game",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist/fsm-game"
+  }
 })
